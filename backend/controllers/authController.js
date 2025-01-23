df1075a55401b5eebf14e84757842334203d557f
Nguyen Quang Huy <nguyenquanghuya3kd@gmail.com>
2025-01-23 19:10:03 +0700

    fix samesite none

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -112,6 +112,7 @@ const adminLogin = async (req, res) => {
       res.cookie("token-admin", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
