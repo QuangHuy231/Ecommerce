3b7f501076f1fbb27da23b69701c78dd2d23e1cf
Nguyen Quang Huy <nguyenquanghuya3kd@gmail.com>
2025-01-23 19:43:05 +0700

    update collection

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -191,31 +191,28 @@ const Collection = () => {
       </div>
 
       {/* Collection */}
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div className="flex-1">
-          <div className="flex justify-between text-base sm:text-2xl mb-4">
-            <Title title1={"ALL"} title2={"COLLECTIONS"} />
-            {/* Sort */}
-            <select
-              onChange={(e) => setSortType(e.target.value)}
-              className="border-2 border-gray-300 text-sm px-2"
-            >
-              <option value="relavent">Sort by: Relavent</option>
-              <option value="low-high">Sort by: Low to High</option>
-              <option value="high-low">Sort by: High to Low</option>
-            </select>
-          </div>
 
-          {/* Products */}
-          <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 gap-y-6">
-            {filterProducts?.map((product) => (
-              <ProductItem key={product._id} product={product} />
-            ))}
-          </div>
+      <div className="flex-1">
+        <div className="flex justify-between text-base sm:text-2xl mb-4">
+          <Title title1={"ALL"} title2={"COLLECTIONS"} />
+          {/* Sort */}
+          <select
+            onChange={(e) => setSortType(e.target.value)}
+            className="border-2 border-gray-300 text-sm px-2"
+          >
+            <option value="relavent">Sort by: Relavent</option>
+            <option value="low-high">Sort by: Low to High</option>
+            <option value="high-low">Sort by: High to Low</option>
+          </select>
         </div>
-      )}
+
+        {/* Products */}
+        <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 gap-y-6">
+          {filterProducts?.map((product) => (
+            <ProductItem key={product._id} product={product} />
+          ))}
+        </div>
+      </div>
 
       {/* Modal */}
       {isOpen && <ModalSelectSize />}
