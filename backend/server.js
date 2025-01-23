38d25c9a05468a46589290142098d1bc6daea800
Nguyen Quang Huy <nguyenquanghuya3kd@gmail.com>
2025-01-23 08:42:48 +0700

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,13 @@ connectDB();
 connectCloudinary();
 
 // config cors for all routes
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(express.json());
 app.use(cookiesparser());
 
